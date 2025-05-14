bbdbfc4d7f691a488a2e7676b1e0ce8202fb0702
WaveDream <wavedream.space@gmail.com>
2025-05-14 00:52:09 -0500

    add xy pad component, bind sliders

--- wavedream.space/src/routes/+page.svelte
+++ wavedream.space/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import Orbs from "./components/Orbs.svelte";
     // import { Pane, Slider } from 'svelte-tweakpane-ui';
 	import Output from "./components/Output.svelte";
+    import XyPad from "./components/XyPad.svelte";
             
     let r = $state(0.5);
     let z = $state(0.5);
@@ -13,9 +14,11 @@
 </script>
 
 <div id='main'>
+
     <div id='orbs'>
         <Orbs />
     </div>
+
     <div id="sidebar">
         <div>
             <div id='output'>
@@ -26,56 +29,26 @@
             <div id="module" class="flex basis-1/16 justify-center items-center bg-gray-800 h-10">
                 <div><h2 class="text-gray-400">SphereHelix</h2></div>
             </div>
-            <div class="basis-13/16"></div>
-            <div id="controls" class="basis-2/16">
-                <div id="sliders">
-                    <div class="flex align-items-center justify-content-center">
-                        <div class="basis-1/4">
-                            <span>Rotate</span>
-                        </div>
-                        <div class="basis-3/4">
-                            <input type="range" min="0" max="1" step="0.001" bind:value={r} class="range range-sm range-primary" />
-                        </div>
-                    </div>
+            <div class="basis-7/16"></div>
+            <div id="controls" class="basis-8/16 w-full">
+                <XyPad bind:rotate={r} bind:zoom={z} />
 
-                    <div class="flex align-items-center justify-content-center">
-                        <div class="basis-1/4">
-                            <span>Zoom</span>
-                        </div>
-                        <div class="basis-3/4">
-                            <input type="range" min="0" max="1" step="0.001" bind:value={z} class="range range-sm range-secondary" />
-                        </div>
-                    </div>
-
-                    <!-- <div class="flex align-items-center justify-content-center">
-                        <div class="basis-1/4">
-                            <span>Scale</span>
-                        </div>
-                        <div class="basis-3/4">
-                            <input type="range" min="0" max="1" step="0.001" bind:value={s} class="range range-sm range-accent" />
-                        </div>
-                    </div>    -->
-                    
-                </div>
             </div>    
-            </div>
+        </div>
     </div>
 </div>
 
 <style>
 
-    #sliders {
+    /* #sliders {
         margin: 10px;
         width: 100%;
-    }
+    } */
 
-    span {
+    /* span {
         color: rgb(100, 122, 141);
         margin: 20px;
-        -webkit-user-select: none; /* Safari */
-        -ms-user-select: none; /* IE 10 and IE 11 */
-        user-select: none; /* Standard syntax */
-    }
+    } */
 
     #main {
         background-color: orange;
---
+++ wavedream.space/src/routes/components/XyPad.svelte
@@ -0,0 +1,74 @@
+<script>
+    let { rotate = $bindable(), zoom = $bindable() } = $props();
+</script>
+
+<div id="container basis-full">
+    <div class="flex flex-col basis-full justify-center">
+        <div id="title" class="flex justify-center align-middle bg-gray-800">
+            <h2>XY Pad</h2>
+        </div>
+
+        <div id="rotate" class= "flex flex-col justify-center items-center basis-20">
+            <div>
+                <span>Rotate</span>
+            </div>
+            <div class="">
+                <input class="rotate" type="range" min="0" max="1" step="0.001" bind:value={rotate}>
+            </div>
+            
+        </div>
+
+        <div id="mid" class="flex justify-center align-middle">
+            <div class="basis-1/4 flex flex-col justify-center items-center">
+                
+                <div class="">
+                    <input type="range" class="inputvert" min="0" max="1" step="0.001" bind:value={zoom}>
+                </div>
+
+                <div class="basis-1/16">
+                    <span class='zoom'>Zoom</span>
+                </div>
+            </div>
+
+            <div class="xypad basis-1/2 bg-gray-900"></div>
+
+            <div class="basis-1/4">
+
+            </div>
+        </div>
+
+        <div class="basis-10">
+
+        </div>
+
+    </div>
+</div>
+
+<style>
+    input {
+        width: 180px;
+    }
+
+    .rotate {
+        width: 180px;
+    }
+
+    .inputvert {
+        appearance: slider-vertical;
+        -moz-appearance: slider-vertical;
+        -webkit-appearance: slider-vertical;
+        height: 180px;
+        width: 20px;
+    }
+
+    .xypad {
+        width: 250px;
+        height: 250px;
+        aspect-ratio: 1;
+        border-radius: 10%;
+        border: 2px solid;
+        border-color: rgb(94, 45, 255);
+    }
+
+
+</style>
